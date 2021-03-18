77e9054e4906c6714d7b415b4303b17d39514d6f
Rotaru Sergey <rsv@arenadata.io>
2021-03-18 21:01:10 +0300

    Corrent version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 setuptools.setup(
     name="ad_ci_tools",
-    version='0.1.2',
+    version='0.1.3',
     author="Rotaru Sergey",
     author_email="rsv@arenadata.io",
     description="Ci library with miscellaneous utils",
