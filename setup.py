dcc1000f0c3876a1c6d9980b7382213dbf58cec7
pisohaka <dkamovsky@gmail.com>
2022-03-23 12:43:19 +1000

    CI-1755 Fix ci_tools for gitlab ci and bump ver
    Gitlab CI change refs to refs/pipelines/id when fetchs source code.
    So we need to fix ci_tools to get repo and branch info
    from env variables.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 setuptools.setup(
     name="ad_ci_tools",
-    version='0.1.6',
+    version='0.1.7',
     author="Rotaru Sergey",
     author_email="rsv@arenadata.io",
     description="Ci library with miscellaneous utils",
